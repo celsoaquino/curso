a15a33f2bdc12fdf6a9d9fc12c981226dea2c749
celsoaquino <celso.aquino@ymail.com>
2019-07-11 19:46:16 -0300

    Testando a primeira classe de dominio -categoria

--- src/main/java/br/com/celsoaquino/curso/resources/CategoriaResources.java
+++ src/main/java/br/com/celsoaquino/curso/resources/CategoriaResources.java
@@ -1,8 +1,12 @@
 package br.com.celsoaquino.curso.resources;
 
+import br.com.celsoaquino.curso.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -10,8 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResources {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "Rest esta funcionando";
+    public List<Categoria> listar(){
+        Categoria c1 = new Categoria(1, "Informatica");
+        Categoria c2 = new Categoria(2, "Escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(c1);
+        lista.add(c2);
+        return lista;
     }
+
+
 }
 
