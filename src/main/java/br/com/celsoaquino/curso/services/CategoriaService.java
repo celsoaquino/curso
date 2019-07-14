7c156e41246ad774cdcfd738d112cbd04bf3d2a8
celsoaquino <celso.aquino@ymail.com>
2019-07-14 17:25:36 -0300

    Correcoes: update CategoriaService e construtor de pagamento

--- src/main/java/br/com/celsoaquino/curso/services/CategoriaService.java
+++ src/main/java/br/com/celsoaquino/curso/services/CategoriaService.java
@@ -33,8 +33,9 @@ public class CategoriaService {
     }
 
     public Categoria update(Categoria obj) {
-        find(obj.getId());
-        return categoriaRepository.save(obj);
+        Categoria newObj = find(obj.getId());
+        updateData(newObj, obj);
+        return categoriaRepository.save(newObj);
     }
 
     public void delete(Integer id){
@@ -58,4 +59,9 @@ public class CategoriaService {
     public Categoria fromDTO(CategoriaDTO objDto){
         return new Categoria(objDto.getId(), objDto.getNome());
     }
+
+    private void updateData(Categoria newObj, Categoria obj) {
+        newObj.setNome(obj.getNome());
+    }
+
 }
